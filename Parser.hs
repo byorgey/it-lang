358bd04416ebdc76878a164e65701f47cc068f95
Brent Yorgey <byorgey@cis.upenn.edu>
2012-11-08 15:00:12 -0500

    more work on parser

--- Parser.hs
+++ Parser.hs
@@ -1,16 +1,19 @@
 module Parser where
 
-import Control.Applicative hiding (many)
-import ItLang
+import           Control.Applicative hiding (many, (<|>))
+import           ItLang
 
-import Text.Parsec
+import           Text.Parsec
+import           Text.Parsec.Language
 import qualified Text.Parsec.Token as P
 
-lexer = P.makeTokenPaser javaDef
+lexer = P.makeTokenParser javaStyle
 
 parens = P.parens lexer
 braces = P.braces lexer
 identifier = P.identifier lexer
+natural = P.natural lexer
+reserved = P.reserved lexer
 symbol = P.symbol lexer
 semi = P.semi lexer
 semiSep = P.semiSep lexer
@@ -23,6 +26,19 @@ toNat n = S (toNat (n-1))
 parseProg = braces (semiSep parseStmt)
 
 parseStmt =
-      Assign <$> identifier <*> (symbol '=' *> parseExp)
+      Assign <$> identifier <*> (symbol "=" *> parseExp)
   <|> Block <$> parseProg
-  <|> If <$> parseBExp <*>
+  <|> If <$> parseBExp
+         <*> (reserved "then" *> parseStmt)
+         <*> (reserved "else" *> parseStmt <* reserved "endif")
+  <|> Repeat <$> (reserved "repeat" *> parseExp)
+             <*> (reserved "times" *> parseStmt <* reserved "done")
+
+-- parseExp =
+--       Lit <$> parseNat
+--   <|> V   <$> identifier
+--   <|> Plus
+
+parseExp = undefined
+
+parseBExp = undefined
