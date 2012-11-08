26124d730e8092cfd983759649bc8bfa8aedb96e
Brent Yorgey <byorgey@cis.upenn.edu>
2012-11-08 13:54:19 -0500

    small start at a parser

---
+++ Parser.hs
@@ -0,0 +1,28 @@
+module Parser where
+
+import Control.Applicative hiding (many)
+import ImpLang
+
+import Text.Parsec
+import qualified Text.Parsec.Token as P
+
+lexer = P.makeTokenPaser javaDef
+
+parens = P.parens lexer
+braces = P.braces lexer
+identifier = P.identifier lexer
+symbol = P.symbol lexer
+semi = P.semi lexer
+semiSep = P.semiSep lexer
+
+parseNat = toNat <$> natural
+
+toNat 0 = Z
+toNat n = S (toNat (n-1))
+
+parseProg = braces (semiSep parseStmt)
+
+parseStmt =
+      Assign <$> identifier <*> (symbol '=' *> parseExp)
+  <|> Block <$> parseProg
+  <|> If <$> parseBExp <*>
